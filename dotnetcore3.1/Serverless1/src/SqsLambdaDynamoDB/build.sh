0eb4cb90eaf7d258011669684aaf8578e733c45a
Dan <78874812+ymwjbxxq@users.noreply.github.com>
2021-09-02 16:22:22 +0200

    update build.sh with linux-musl-x64

--- dotnetcore3.1/Serverless1/src/SqsLambdaDynamoDB/build.sh
+++ dotnetcore3.1/Serverless1/src/SqsLambdaDynamoDB/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-dotnet publish ./SqsLambdaDynamoDB.csproj --configuration "Release" --framework "netcoreapp3.1" /p:PublishReadyToRun=true /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64 --self-contained false
+--configuration "Release" --framework "netcoreapp3.1" /p:PublishReadyToRun=true /p:GenerateRuntimeConfigurationFiles=true --runtime linux-musl-x64 --self-contained false || exit 1
 # Create a new directory where we will put deployment package
 mkdir -p /package
 # # Go to bin folder with built project
-cd ./bin/Release/netcoreapp3.1/publish
+cd ./bin/Release/netcoreapp3.1/linux-musl-x64/publish
 # Archive deployment package and store it to the specified folder
 zip -9 -r /package/deployment-archive.zip . || exit 1
 # # # Create the output directory in the mounted volume
